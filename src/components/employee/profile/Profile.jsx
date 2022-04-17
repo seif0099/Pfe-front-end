a7baeaccc5fcfac1cff3a527a5e470b160537e69
seif0099 <seifbenhassen99@gmail.com>
2022-04-17 02:15:31 +0200

    ahla

--- src/components/employee/profile/Profile.jsx
+++ src/components/employee/profile/Profile.jsx
@@ -1,20 +1,29 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./profile.css"
+import History from "../../authentication/sign-up/History";
 
 function Profile() {
-    const [name, setName] = useState('');
+    const [userInfo, setUserInfo] = useState({});
+
+    const [nom, setNom] = useState('');
+    const [prenom, setPrenom] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-   
+   const[matricule,setMatricule]=useState('')
+   const[classe,setClasse]=useState('')
+   const[username,setUsername]=useState('')
     // States for checking the errors
     const [submitted, setSubmitted] = useState(false);
     const [error, setError] = useState(false);
    
     // Handling the name change
     const handleName = (e) => {
-      setName(e.target.value);
-      setSubmitted(false);
+      setNom(e.target.value);
     };
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+        setSubmitted(false);
+      };
    
     // Handling the email change
     const handleEmail = (e) => {
@@ -27,17 +36,29 @@ function Profile() {
       setPassword(e.target.value);
       setSubmitted(false);
     };
-   
+    const handleClasse = (e) => {
+        setClasse(e.target.value);
+        setSubmitted(false);
+      };
+      const handleMatricule = (e) => {
+        setMatricule(e.target.value);
+        setSubmitted(false);
+      };
+      const handlePrenom = (e) => {
+        setPrenom(e.target.value);
+        setSubmitted(false);
+      };
     // Handling the form submission
     const handleSubmit = (e) => {
       e.preventDefault();
-      if (name === '' || email === '' || password === '') {
+      if (nom === '' || email === '' || password === '') {
         setError(true);
       } else {
         setSubmitted(true);
         setError(false);
       }
     };
+
    
     // Showing success message
     const successMessage = () => {
@@ -47,7 +68,7 @@ function Profile() {
           style={{
             display: submitted ? '' : 'none',
           }}>
-          <h1>User {name} successfully registered!!</h1>
+          <h1>User {userInfo?.nom} successfully updated!!</h1>
         </div>
       );
     };
@@ -64,6 +85,36 @@ function Profile() {
         </div>
       );
     };
+    useEffect(() => {
+        if(JSON.parse(localStorage.getItem("user-info"))){
+          const { user } = JSON.parse(localStorage.getItem("user-info"));
+          setUserInfo(user);
+        }
+  
+    }, []);
+    async function updateProfile(e) {
+        e.preventDefault();
+    
+        let item = {
+          nom,
+          prenom,
+          classe,
+          matricule,
+          username,
+          email,
+          password,
+          userid: userInfo?._id,
+        };
+        let result = await fetch("http://localhost:9000/UpdateUser", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(item),
+        });
+        let results = await result.json();
+      }
   return (
    
  
@@ -81,18 +132,30 @@ function Profile() {
  
       <form>
         {/* Labels and inputs for form data */}
-        <label className="label">Name</label>
-        <input onChange={handleName} className="input"
-          value={name} type="text" />
+        <label className="label">Username</label>
+        <input onChange={handleUsername} className="inputt"
+          value={userInfo?.Username}  type="text"  />
+        <label className="label">Nom</label>
+        <input onChange={handleName} className="inputt"
+          value={userInfo?.nom} type="text" />
+            <label className="label">Prénom</label>
+        <input onChange={handlePrenom} className="inputt"
+          value={userInfo?.prenom} type="text" />
  
         <label className="label">Email</label>
-        <input onChange={handleEmail} className="input"
-          value={email} type="email" />
+        <input onChange={handleEmail} className="inputt"
+          value={userInfo?.email} type="email" />
  
         <label className="label">Password</label>
-        <input onChange={handlePassword} className="input"
-          value={password} type="password" />
+        <input onChange={handlePassword} className="inputt"
+          value={userInfo?.password} type="password" />
  
+ <label className="label">Classe</label>
+        <input onChange={handleClasse} className="inputt"
+          value={userInfo?.classe} type="text" />
+           <label className="label">Matricule</label>
+        <input onChange={handleMatricule} className="inputt"
+          value={userInfo?.matricule} type="password" />
         <button onClick={handleSubmit} className="btn" type="submit">
           Submit
         </button>
