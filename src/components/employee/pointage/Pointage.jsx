eee12d69891a392b518e57c2814fc976e626b7a1
Your Name <you@example.com>
2022-04-18 02:18:02 +0100

    aaa

--- src/components/employee/pointage/Pointage.jsx
+++ src/components/employee/pointage/Pointage.jsx
@@ -7,13 +7,10 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import Box from '@mui/material/Box';
 function Pointage() {
-    const [userInfo, setUserInfo] = useState({});
-  const [fromDate, setFromDate] = useState("");
-  const [toDate, setToDate] = useState("");
+  const [userInfo, setUserInfo] = useState({});
   const dateObj = new Date();
-
-  const [dates, setDate] = useState(dateObj)
-  ;
+  const [dates, setDate] = useState(dateObj);
+  const [pointages, setPointages] = useState();
 
   useEffect(() => {
 	  if(JSON.parse(localStorage.getItem("user-info"))){
@@ -26,13 +23,10 @@ function Pointage() {
     e.preventDefault();
 
     let item = {
-      fromDate,
-      toDate,
-      nom: userInfo?.nom,
-      prenom: userInfo?.prenom,
+      pDate: dates, 
       userid: userInfo?._id,
     };
-    let result = await fetch("http://localhost:9000/pointage", {
+    let result = await fetch("http://localhost:9000/getpointage", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -40,16 +34,10 @@ function Pointage() {
       },
       body: JSON.stringify(item),
     });
+    console.log(result)
     let results = await result.json();
   }
-    function fullDate() {
-        var tempDate = new Date();
-        var date = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() +' '+ tempDate.getHours()+':'+ tempDate.getMinutes()+':'+ tempDate.getSeconds();
-        const currDate = date;
-        return (
-          <p>{currDate}</p>
-        );
-      }
+
   return (
   
    <div className='container'>
@@ -72,23 +60,7 @@ function Pointage() {
                 </tr>
               </thead>
               <tbody>
-                
-               
-                <tr>
-                  <th>Sem 1</th>
-                </tr>
-                <tr>
-                  <th>Sem 2</th>
-                  
-                </tr>
-                <tr>
-                  <th>Sem 3</th>
-                  
-                </tr>
-                <tr>
-                  <th>Sem 4</th>
-                 
-                </tr>
+             
               </tbody>
             </table>
           </div>
