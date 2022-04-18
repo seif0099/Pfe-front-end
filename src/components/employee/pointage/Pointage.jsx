db5ddaa2ab328b9713804540dbe75f6e880fb03e
Your Name <you@example.com>
2022-04-18 01:48:25 +0100

    updates

--- src/components/employee/pointage/Pointage.jsx
+++ src/components/employee/pointage/Pointage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import "./pointage.css"
 import { useEffect,useState } from 'react';
-
-
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import Box from '@mui/material/Box';
 function Pointage() {
     const [userInfo, setUserInfo] = useState({});
   const [fromDate, setFromDate] = useState("");
   const [toDate, setToDate] = useState("");
+  const dateObj = new Date();
+
+  const [dates, setDate] = useState(dateObj)
+  ;
 
   useEffect(() => {
 	  if(JSON.parse(localStorage.getItem("user-info"))){
@@ -45,67 +52,65 @@ function Pointage() {
       }
   return (
   
-   <div className="limiter" >
-       <div className="wrapper">
-      <div className="inner" >
-        <form>
-          <h3>pointage</h3>
-          <div className="form-row">
-            <div className="form-wrapper">
-              <label htmlFor="">Nom *</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nom"
-                value={userInfo?.nom}
-				disabled
-              />
-            </div>
-            <div className="form-wrapper">
-              <label htmlFor="">Prénom *</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Prénom"
-                value={userInfo?.prenom}
-				disabled
-              />
-            </div>
+   <div className='container'>
+<div className="row py-5">
+
+    <div className="col-lg-10 mx-auto">
+          <div className="table-responsive">
+            <table className="table table-striped table-bordered" id="example" >
+              <thead>
+                <tr>
+                  <th><p>{dates.getMonth()}/
+    {dates.getFullYear()}</p>
+</th>
+                  <th>Lundi</th>
+                  <th>Mardi</th>
+                  <th>Mercredi</th>
+                  <th>Jeudi</th>
+                  <th>Vendredi</th>
+                  <th>Samedi</th>
+                </tr>
+              </thead>
+              <tbody>
+                
+               
+                <tr>
+                  <th>Sem 1</th>
+                </tr>
+                <tr>
+                  <th>Sem 2</th>
+                  
+                </tr>
+                <tr>
+                  <th>Sem 3</th>
+                  
+                </tr>
+                <tr>
+                  <th>Sem 4</th>
+                 
+                </tr>
+              </tbody>
+            </table>
           </div>
-
-          <div className="form-row last">
-            <div className="form-wrapper">
-              <label htmlFor="">Date d'entrée*</label>
-              <input
-                type="time"
-                className="form-control"
-                onChange={(e) => setFromDate(e.target.value)}
-              />
-              <i className="zmdi zmdi-chevron-down"></i>
-            </div>
-            <div className="form-wrapper">
-              <label htmlFor="">À *</label>
-              <input
-                type="time"
-                className="form-control"
-                onChange={(e) => setToDate(e.target.value)}
-              />
-
-              <i className="zmdi zmdi-chevron-down"></i>
-            </div>
-          </div>
-          <div className="form-wrapper">
-         
-          </div>
-          <button data-text="Confirmer" onClick={makePresence}>
-            <span>confirmer</span>
-          </button>
-        </form>
-      </div>
-    </div>
-
-</div>
-
+          <div className='monthPicker'>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box m={2}>
+        <DatePicker
+          inputFormat="yyyy-MM"
+          views={['year', 'month']}
+          label="Mois / Année"
+          minDate={new Date('2012-03-01')}
+          maxDate={new Date('2023-06-01')}
+          value={dates}
+          onChange={setDate}
+          renderInput={(params) => <TextField {...params} helperText={null} />}
+        />
+      </Box>
+    </LocalizationProvider>
+        </div>
+        </div>
+  </div>
+  </div>
   )
 }
 
