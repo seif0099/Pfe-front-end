d77016fdae788c7539d0e2cd5f3fff287fb56bf4
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-05-02 04:46:20 +0100

    aa

--- src/components/admin/mission/Mission.jsx
+++ src/components/admin/mission/Mission.jsx
@@ -1,14 +1,13 @@
 import React,{useState,useEffect} from 'react'
 import "./mission.css"
+import { useFormik } from 'formik';
 
 function Mission() {
  
   var [res,setRes]=useState([{}]);
-  const [userid,setUserid]=useState("");
-  const[destination,setDest]=useState("");
-  const[dateDepart,setDateDep]=useState("");
-  const[dateRetour,setDateRet]=useState("");
-const[objectif,setObjectif]=useState("");
+  const [errorResponse, setError] = useState("");
+  const [successResponse, setSuccess] = useState("");
+
 
 
   useEffect(() => {
@@ -26,16 +25,14 @@ const[objectif,setObjectif]=useState("");
     }
     fetcha();
   }, []);
- async function insertion (){
+ async function insertion (values){
 	  let item = {
-		
-		objectifMission:objectif,
-		dateRetour:dateRetour,
-		dateDepart:dateDepart,
-		destination:destination,
+		objectifMission:values.objectifMission,
+		dateRetour:values.dateRetour,
+		dateDepart:values.dateDepart,
+		destination:values.destination,
 	  };
-	  console.log(item)
-	  const URL = "http://localhost:9000/createmission?id="+userid
+	  const URL = "http://localhost:9000/createmission?id="+values.userid
 	  let result = await fetch(URL, {
 		method: "POST",
 		headers: {
@@ -45,15 +42,56 @@ const[objectif,setObjectif]=useState("");
 		body: JSON.stringify(item),
 	  });
       
-	  let results = await result.json();
+	  if(result.status == 200){
+		setSuccess("Demande envoyé avec succés")
+	  setError(null)
+	}
+	else{
+	  setSuccess(null)
+	  setError(result)
+	}
 
 	}
-    function handleUserid(e){
-setUserid(e.target.value)
-console.log(e.target.value);
-    }
+  
 
-
+	function validate(values) {
+		const errors = {};
+		if (!values.userid) {
+		  errors.userid = "* Le champ employée est obligatoire";
+		}
+		if(!values.objectifMission){
+			errors.objectifMission = "* Le champ objectif mission est obligatoire";
+		}
+		if(!values.dateDepart){
+			errors.dateDepart = "* Le champ date départ est obligatoire";
+		}
+		if(!values.dateRetour){
+			errors.dateRetour = "* Le champ date retour est obligatoire";
+		}
+		if(!values.destination){
+			errors.destination = "* Le champ destination est obligatoire";
+		}
+		return errors;
+	  }
+	
+	  const {
+		handleSubmit,
+		handleChange,
+		touched,
+		errors,
+	  } = useFormik({
+		initialValues: {
+			userid: "",
+			dateRetour: "",
+			dateDepart: "",
+			objectifMission: "",
+			destination: ""
+		},
+		validate,
+		onSubmit: (values) => {
+			insertion(values)
+		},
+	  });
 
   return (
     <div className="wrapper">
@@ -63,56 +101,76 @@ console.log(e.target.value);
 					<div className="form-row">
 						<div className="form-wrapper">
 							
-                            <select className='form-control' onChange={handleUserid}>
+                            <select className='form-control' name="userid" onChange={handleChange}>
                               <option selected value="0">Choisir l'employée</option>
                               {res.map(({ nom, prenom,_id }, index) => <option value={_id} > {nom} {prenom} </option>)}
                             </select>
                            
 						</div>
 					
+					
 					</div>
-				
+					{touched.userid && errors.userid
+        						? <p className="errors">{errors.userid}</p>
+        						: null}
 					<div className="form-row last">
 						<div className="form-wrapper" >
 							<label htmlFor="">Date de départ</label>
-				   	<input type="date" className="form-control"  onChange={(e) => setDateDep(e.target.value)} />
+				   	<input type="date" className="form-control"  name="dateDepart" onChange={handleChange} />
 							<i className="zmdi zmdi-chevron-down"></i>
 						</div>
 					
-						
+					
           
 				</div>
 				
-
+				{touched.dateDepart && errors.dateDepart
+        						? <p className="errors">{errors.dateDepart}</p>
+        						: null}
                 <div className="form-row last">
 						<div className="form-wrapper" >
 							<label htmlFor="">Date de retour</label>
-				   	<input type="date" className="form-control"  onChange={(e) => setDateRet(e.target.value)} />
+				   	<input type="date" className="form-control"  name="dateRetour" onChange={handleChange} />
 							<i className="zmdi zmdi-chevron-down"></i>
 						</div>
 					
 						
           
 				</div>
+				{touched.dateRetour && errors.dateRetour
+        						? <p className="errors">{errors.dateRetour}</p>
+        						: null}
                 <div className="form-row last">
 						<div className="form-wrapper" >
 							<label htmlFor="">Destination</label>
-				   	<input type="text" className="form-control"  onChange={(e) => setDest(e.target.value)} />
+				   	<input type="text" className="form-control"  name="destination" onChange={handleChange} />
 							<i className="zmdi zmdi-chevron-down"></i>
 						</div>
+					
 					</div>
+					{touched.destination && errors.destination
+        						? <p className="errors">{errors.destination}</p>
+        						: null}
                         <div className="form-row last">
 						<div className="form-wrapper" >
 							<label htmlFor="">Objectif</label>
-				   	<input type="text" className="form-control"  onChange={(e) => setObjectif(e.target.value)} />
+				   	<input type="text" className="form-control"  name="objectifMission" onChange={handleChange} />
 							<i className="zmdi zmdi-chevron-down"></i>
 						</div>
+					
 				</div>
-     
-					<button data-text="Confirmer la présence" type="button" onClick={insertion} className='form-control button1'>
+				{touched.objectifMission && errors.objectifMission
+        						? <p className="errors">{errors.objectifMission}</p>
+        						: null}
+					<button data-text="Confirmer la présence" type="button" onClick={handleSubmit} className='form-control button1'>
 						Confirmer 
 					</button>
-                 
+					{successResponse
+        						? <h1 className="serverSuccess">{successResponse}</h1>
+        						: null}
+					{errorResponse
+        						? <p className="errors">{errorResponse}</p>
+        						: null}
 				</form>
 			</div>
 		</div>
--- src/components/admin/sanction/Sanction.jsx
+++ src/components/admin/sanction/Sanction.jsx
@@ -1,13 +1,10 @@
-import { ConsoleSqlOutlined } from '@ant-design/icons';
 import React,{useState,useEffect} from 'react'
+import { useFormik } from 'formik';
 
 function Sanction() {
 	var [res,setRes]=useState([{}]);
-	const [userid,setUserid]=useState("");
-	const obj = new Date()
-	const [fromDate,setfromDate]=useState("");
-	const [toDate,settoDate]=useState("");
-	const [raison,setRaison]=useState("");
+	const [errorResponse, setError] = useState("");
+	const [successResponse, setSuccess] = useState("");
 
 	useEffect(() => {
 		const fetcha = async () => {
@@ -25,18 +22,10 @@ function Sanction() {
 		}
 		fetcha();
 	  }, []);
-	function handleUserid(e){
-		setUserid(e.target.value)
-    }
-	async function insertion (){
+
+	async function insertion (values){
 	
-		let item = {
-		  userid: userid,
-		  reasonForSanction: raison,
-		  FromDate: fromDate,
-		  ToDate: toDate
-		};
-		console.log(item)
+		
 		const URL = "http://localhost:9000/addsanction"
 		let result = await fetch(URL, {
 		  method: "POST",
@@ -44,11 +33,52 @@ function Sanction() {
 			"Content-Type": "application/json",
 			Accept: "application/json",
 		  },
-		  body: JSON.stringify(item),
+		  body: JSON.stringify(values),
 		});
-		let results = await result.json();
+		if(result.status == 200){
+			setSuccess("Demande envoyé avec succés")
+		  setError(null)
+		}
+		else{
+		  setSuccess(null)
+		  setError(result)
+		}
   
 	  }
+	  function validate(values) {
+		const errors = {};
+		if (!values.userid) {
+		  errors.userid = "* Le champ employée est obligatoire";
+		}
+		if(!values.fromDate){
+			errors.fromDate = "* Le champ heure début est obligatoire";
+		}
+		if(!values.toDate){
+			errors.toDate = "* Le champ heure fin est obligatoire";
+		}
+		if(!values.reasonForSanction){
+			errors.reasonForSanction = "* Le champ raison est obligatoire";
+		}
+		return errors;
+	  }
+	
+	  const {
+		handleSubmit,
+		handleChange,
+		touched,
+		errors,
+	  } = useFormik({
+		initialValues: {
+			userid: "",
+			fromDate: "",
+			toDate: "",
+			reasonForSanction: ""
+		},
+		validate,
+		onSubmit: (values) => {
+			insertion(values)
+		},
+	  });
   return (
     <div>
         <div className="wrapper">
@@ -57,39 +87,57 @@ function Sanction() {
 					<h3>Sanction</h3>
 					<div className="form-row">
 						<div className="form-wrapper">
-						<select className='form-control' onChange={handleUserid}>
+						<select className='form-control' name="userid" onChange={handleChange}>
                               <option selected value="0">Choisir l'employée</option>
                               {res.map(({ nom, prenom,_id }, index) => <option value={_id} > {nom} {prenom} </option>)}
                             </select>
 						</div>
 					
 					</div>
+					{touched.userid && errors.userid
+        						? <p className="errors">{errors.userid}</p>
+        						: null}
+					
                     <div className="form-row">
 						<div className="form-wrapper">
 							<label htmlFor="">Raison de sanction  *</label>
-							<input type="text" className="form-control" placeholder="Raison" onChange={(e) => setRaison(e.target.value)}/>
+							<input type="text" className="form-control" placeholder="Raison" name="reasonForSanction" onChange={handleChange}/>
 						</div>
-					
+						
 					</div>
-				
+					{touched.reasonForSanction && errors.reasonForSanction
+        						? <p className="errors">{errors.reasonForSanction}</p>
+        						: null}
 					<div className="form-row last">
 						<div className="form-wrapper">
 							<label htmlFor="">À partir de  *</label>
-				   	<input type="date" className="form-control" onChange={(e) => setfromDate(e.target.value)}/>
+				   	<input type="date" className="form-control" name="fromDate" onChange={handleChange}/>
 							<i className="zmdi zmdi-chevron-down"></i>
+							{touched.fromDate && errors.fromDate
+        						? <p className="errors">{errors.fromDate}</p>
+        						: null}
 						</div>
 						<div className="form-wrapper">
 							<label htmlFor="">À *</label>
-              <input type="date" className="form-control" onChange={(e) => settoDate(e.target.value)}/>
+              <input type="date" className="form-control" name="toDate" onChange={handleChange}/>
 
 							<i className="zmdi zmdi-chevron-down"></i>
+							{touched.toDate && errors.toDate
+        						? <p className="errors">{errors.toDate}</p>
+        						: null}
 						</div>
           
 				</div>
         
-					<button data-text="Confirmer" className="form-control button1" type="button" onClick={insertion}>
+					<button data-text="Confirmer" className="form-control button1" type="button" onClick={handleSubmit}>
 						Confirmer
 					</button>
+					{successResponse
+        						? <h1 className="serverSuccess">{successResponse}</h1>
+        						: null}
+					{errorResponse
+        						? <p className="errors">{errorResponse}</p>
+        						: null}
 				</form>
 			</div>
 		</div>
